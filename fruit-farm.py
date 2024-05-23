e8e1334b29791e767f908470ac9c83c8aa963d69
DiFarGon <diogo.faria.goncalves@tecnico.ulisboa.pt>
2024-05-23 17:44:58 +0100

    added plotting for easier comparison between agents. added a progress bar while iterating. default max iterations per environment execution is now 500.

--- fruit-farm.py
+++ fruit-farm.py
@@ -2,16 +2,19 @@ import time
 from typing import Sequence
 from gym import Env
 import numpy as np
+from tqdm import tqdm
 
 from environment import Environment
 from wrappers import SingleAgentWrapper
+
+from utils import compare_results
 
 from agents import Agent, CooperativeAgent, GreedyAgent, RandomAgent
 
 def run_single_agent(environment: Env, agent: Agent, n_episodes: int) -> np.ndarray:
   results = np.zeros((n_episodes, 2))
 
-  for episode in range(n_episodes):
+  for episode in tqdm(range(n_episodes), desc=f'Running {agent.name}'):
     steps = 0
     terminal = False
     observations = environment.reset()
@@ -30,7 +33,7 @@ def run_single_agent(environment: Env, agent: Agent, n_episodes: int) -> np.ndar
 def run_multi_agent(environment: Env, agents: Sequence[Agent], n_episodes: int, visual: bool) -> np.ndarray:
   results = np.zeros((n_episodes, 2))
 
-  for episode in range(n_episodes):
+  for episode in tqdm(range(n_episodes), desc=f'Running {agents[0].name[:-2]} agents'):
     steps = 0
     terminals = [False for _ in agents]
     observations = environment.reset()
@@ -50,6 +53,7 @@ def run_multi_agent(environment: Env, agents: Sequence[Agent], n_episodes: int, 
     results[episode] = [environment.total_episode_reward[0], steps]
     environment.close()
 
+  print()
   return results
 
 if __name__ == '__main__':
@@ -57,20 +61,16 @@ if __name__ == '__main__':
  
   teams = {
     'random': [RandomAgent(f'random_{i}', environment.action_space[i].n) for i in range(4)],
-    #'greedy': [GreedyAgent(f'greedy_{i}', environment.action_space[i].n, 4) for i in range(4)],
-    #'cooperative': [CooperativeAgent(f'cooperative_{i}', environment.action_space[i].n, 4, 5) for i in range(4)],
+    'greedy': [GreedyAgent(f'greedy_{i}', environment.action_space[i].n, 4) for i in range(4)],
+    'cooperative': [CooperativeAgent(f'cooperative_{i}', environment.action_space[i].n, 4, 5) for i in range(4)],
   }
 
-  results = {}
+  steps = {}
   for team, agent in teams.items():
     result = run_multi_agent(environment, agent, 100, visual=False)
-    results[team] = {
-      'reward': result[:, 0].mean(),
-      'steps': result[:, 1].mean()
-    }
+    steps[team] = result[:, 1]
 
-  print(results)
-
+  compare_results(steps, title='Mean steps per episode', colors=['orange', 'green', 'blue'])
 
 # sacrificial lamb: if apples fall below a threshold, the agent the furthest away
 # from apples will sacrifice itself to feed the others
